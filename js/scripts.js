ae6b67900ca9677aaf6f2dab4bc3f8a520e9d328
Maciej Czerwinski <maciejcz@wp.pl>
2017-03-22 09:42:52 +0100

    add doctype, change i in for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function rysujChoinke(parametr) {
-	for(var i = 0; i <= parametr; i++) {
+	for(var i = 1; i <= parametr; i++) {
 		var star = '';
 		for(j = 0; j < (i * 2 - 1); j++) {
 			var star = star + '*';
